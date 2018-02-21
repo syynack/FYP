ed53134f79202ca62340c5dabbefae7abfcd9d07
Matt Lovatt <mlhome96@gmail.com>
2018-02-21 19:07:58 +0000

    Added cisco_ios_get_bgp_summary and test 5

--- moss/framework/devops/cisco_ios/__init__.py
+++ moss/framework/devops/cisco_ios/__init__.py
@@ -3,5 +3,6 @@ __all__ = [
     'cisco_ios_apply_configuration',
     'cisco_ios_show_interfaces',
     'cisco_ios_check_configuration',
-    'cisco_ios_ping'
+    'cisco_ios_ping',
+    'cisco_ios_get_bgp_summary'
 ]
